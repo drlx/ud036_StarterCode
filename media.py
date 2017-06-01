3dda4babf00d74896ce51a5fa6ad67bb5e02dbc9
drlx <yazid129@gmail.com>
2017-06-01 13:21:46 -0400

    PEP8 Formatting
    Formatting changes

--- media.py
+++ media.py
@@ -8,6 +8,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    def show_trailer(self):
+    def show_trailer(self):  # Create function for launching and testing trailers
         webbrowser.open(self.trailer_youtube_url)
     
